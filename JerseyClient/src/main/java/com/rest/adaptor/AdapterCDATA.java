afa0fef748f07279d8b6124e8649afadfb27adca
jarulraj666 <jarulraj6666@gmail.com>
2019-04-29 17:04:01 +0530

    Marshall logic change

--- JerseyClient/src/main/java/com/rest/adaptor/AdapterCDATA.java
+++ JerseyClient/src/main/java/com/rest/adaptor/AdapterCDATA.java
@@ -6,11 +6,11 @@ public class AdapterCDATA extends XmlAdapter<String, String> {
 
     @Override
     public String marshal(String arg0) throws Exception {
-        return "![CDATA[" + arg0 + "]]";
+        return arg0;
     }
     @Override
     public String unmarshal(String arg0) throws Exception {
-        return arg0;
+    	return "![CDATA[" + arg0 + "]]";
     }
 
 }
